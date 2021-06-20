cb670275250afd8e579f9cf865f1e87c9f39aee4
kan-fumihito <kan_f1204@docomo.ne.jp>
2021-06-20 10:46:39 +0900

    tele 遠隔操作

--- tele.js
+++ tele.js
@@ -4,7 +4,13 @@ var ws;
     ws.onopen = function(e) {
         document.getElementById("turn").innerText;
         document.getElementById("msg").innerText = "Connection Start";
+        var advertise = {
+            "op": "advertise",
+            "topic": "/cmd_vel",
+            "type": "geometry_msgs/Twist",
+        };
 
+        ws.send(JSON.stringify(advertise));
     }
     ws.onerror = function(e) {
         document.getElementById("msg").innerText = "Error";
@@ -23,7 +29,50 @@ var ws;
 
 var count = 0;
 
-function send() {
-    var msg = "Message: " + String(count++);
-    ws.send(msg);
+var lx = [1, 1, 1, 0, 0, 0, -1, -1, -1, ];
+/*var ly = [
+
+];
+var lz = [
+
+];
+var ax = [
+
+];
+var ay = [
+
+];*/
+var az = [1, 0, -1, 1, 0, -1, -1, 0, 1, ];
+
+function send(idx) {
+    /*var msg = {
+        angular: {
+            x: 1,
+            y: 0,
+            z: 0
+        },
+        linear: {
+            x: 0,
+            y: 0,
+            z: 0
+        }
+    };*/
+    var msg = {
+        "op": "publish",
+        "topic": "/cmd_vel",
+        "msg": {
+            angular: {
+                x: 0,
+                y: 0,
+                z: az[idx] * 0.25
+            },
+            linear: {
+                x: lx[idx] * 0.125,
+                y: 0,
+                z: 0
+            }
+        }
+    };
+    console.log(idx);
+    ws.send(JSON.stringify(msg));
 }
